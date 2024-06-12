1307524f44bcd3c8ab2460f462f67c47b9303ca4
I3UX_ <matheo.plasz@gmail.com>
2024-06-12 12:11:24 +0200

    ansi constant for logger implemented

---
+++ internal/ansi.go
@@ -0,0 +1,15 @@
+package internal
+
+const (
+	//Log colors
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+	ValidColor   = "\033[1;32m%s\033[0m"
+	//Colors
+	Green  = "\033[0;32m%s\033[0m"
+	Purple = "\033[1;35m%s\033[0m"
+	B_Cyan = "\033[1;36m%s\033[0m"
+)
